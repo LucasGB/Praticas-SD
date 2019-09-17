976d3178c0235b1a2a891237c8cd737ff3323b80
Lucas Guedes Barboza <lucasguedesbarboza@gmail.com>
2019-09-16 23:49:30 -0300

    Questao 01.

--- src/atividade_02/exercicio_01/Peer.java
+++ src/atividade_02/exercicio_01/Peer.java
@@ -3,39 +3,46 @@ package atividade_02.exercicio_01;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.nio.charset.Charset;
 import javax.swing.JOptionPane;
 
-
 public class Peer {
-    public static void main(String args[]){ 
-    	DatagramSocket dgramSocket = null;
-        try{
+
+    public static void main(String args[]) {
+        DatagramSocket dgramSocket = null;
+        int resp = 0;
+
+        try {
             dgramSocket = new DatagramSocket();
-            
-            System.out.println("Local port: " + dgramSocket.getLocalPort());                        
-            
+
+            System.out.println("Local port: " + dgramSocket.getLocalPort());
+
             InputThread in = new InputThread(dgramSocket);
             in.start();
             
-            while(true){
-                String dstIP = JOptionPane.showInputDialog("IP Destino?");
-                int dstPort = Integer.parseInt(JOptionPane.showInputDialog("Porta Destino?"));
-                
+            String nickName = JOptionPane.showInputDialog("Username?");
+            String dstIP = JOptionPane.showInputDialog("IP Destino?");
+            int dstPort = Integer.parseInt(JOptionPane.showInputDialog("Porta Destino?"));
+
+            do {
+
                 byte[] buffer = new byte[1024];
 
-                DatagramPacket dgramPacket = new DatagramPacket(buffer, buffer.length);
-                dgramSocket.receive(dgramPacket);
+                String messageBody = JOptionPane.showInputDialog("Mensagem: ");
+                String message = new String().format("%d%s%d%s", (byte) nickName.length(), nickName, (byte) messageBody.length(), messageBody, Charset.forName("UTF-8"));
+                DatagramPacket messagePacket = new DatagramPacket(message.getBytes(), message.getBytes().length, InetAddress.getByName(dstIP), dstPort);
+                dgramSocket.send(messagePacket);
 
-                DatagramPacket reply = new DatagramPacket(dgramPacket.getData(),
-                        dgramPacket.getLength(), dstIP, dstPort);
-                
-                DatagramPacket r = new DatagramPacket(buffer, dstPort, address)
-                dgramSocket.send(reply);
-            }
-        }catch (SocketException e){
+                resp = JOptionPane.showConfirmDialog(null, "Nova mensagem?",
+                        "Continuar", JOptionPane.YES_NO_OPTION);
+
+            } while (resp != JOptionPane.NO_OPTION);
+
+        } catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("IO: " + e.getMessage());
         } finally {
             dgramSocket.close();
